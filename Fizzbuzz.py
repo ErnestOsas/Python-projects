4d794c11952b75b180ad30548d31d92242fab060
Ernest Akibor <akiborernest@gmail.com>
2021-01-22 16:10:58 +0100

    my project

---
+++ Fizzbuzz.py
@@ -0,0 +1,13 @@
+for i in range(101):
+    if i%3 == 0 and i%5 == 0:
+        print("FizzBuzz")
+    elif i%3 == 0:
+        print("Fizz")
+    elif i%5 == 0:
+        print("Buzz")
+    elif i%2 == 0:
+        print(i)
+    else:
+        print("Prime")
+
+
